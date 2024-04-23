2067d46e2fd45a819d2fd94485065319118e61fa
Vladimir Moiseev <memmfizwork@gmail.com>
2024-04-23 17:31:07 +0300

    Updated date format

--- main/db.py
+++ main/db.py
@@ -13,19 +13,34 @@ def create_conn():
 def save_text(user_id: int, first_name: str, last_name: str, username: str, messages: dict) -> None:
     conn = create_conn()
     cur = conn.cursor()
+
     cur.execute('SELECT COUNT(*) FROM birthdays')
-    messages['id'] = cur.fetchone()[0] + 1
+    count = cur.fetchone()
+    if count is not None:
+        messages['id'] = count[0] + 1
+
     messages['user_name'] = f"{first_name} {last_name}" if last_name else first_name
     messages['user_telegram_id'] = user_id
     messages['user_telegram_name'] = username
     messages['last_modified'] = datetime.now().isoformat(timespec='seconds')
-    insert_query = f"""
-        INSERT INTO birthdays (birth_person, birth_month, birth_date, birth_age, sex, id, user_name, user_telegram_id, user_telegram_name, last_modified)
-        VALUES ('{messages['birth_person']}', '{messages['birth_month']}', {messages['birth_date']}, {messages['birth_age']}, 
-        '{messages['sex']}',  {messages['id']}, '{messages['user_name']}', {messages['user_telegram_id']}, 
-        '{messages['user_telegram_name']}', '{messages['last_modified']}');
+
+    # формирование даты рождения
+    birth_date = messages.get('birth_date')
+
+    # Сохраняем дату в базе данных
+    query = """
+        INSERT INTO birthdays (
+            birth_person, birth_date, sex, id, user_name, 
+            user_telegram_id, user_telegram_name, last_modified
+        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
     """
-    cur.execute(insert_query)
+    values = (
+        messages['birth_person'], birth_date, messages['sex'],
+        messages['id'], messages['user_name'], messages['user_telegram_id'],
+        messages['user_telegram_name'], messages['last_modified']
+    )
+    cur.execute(query, values)
     conn.commit()
+
     cur.close()
     conn.close()
--- main/handlers/message.py
+++ main/handlers/message.py
@@ -1,80 +1,77 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import CallbackContext
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from datetime import datetime, date
 
 
-def handle_message(update: Update, context: CallbackContext) -> None:
+def is_leap(year: int) -> bool:
+    # функция проверки на високосный год
+    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+        return True
+    return False
+
+
+def handle_message(update, context):
     text = update.message.text
-    user_id = update.effective_user.id
-
     if 'stage' not in context.user_data:
         context.user_data['stage'] = ''
 
+    # условие обработки ввода имени
     if context.user_data['stage'] == 'awaiting_birth_person':
-        context.user_data['stage'] = 'awaiting_birth_age'
-        context.user_data['birth_person'] = text
-        keyboard = [[InlineKeyboardButton("Пропустить", callback_data='skip')]]
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        context.bot.send_message(chat_id=update.effective_chat.id, text='Введите год рождения',
-                                 reply_markup=reply_markup)
+        if len(text) > 256:
+            context.bot.send_message(chat_id=update.effective_chat.id, text='Ваше имя слишком длинное.')
+        else:
+            context.user_data['birth_person'] = text
+            context.user_data['stage'] = 'awaiting_birth_age'
+            keyboard = [[InlineKeyboardButton("Пропустить", callback_data='skip')]]
+            reply_markup = InlineKeyboardMarkup(keyboard)
+            context.bot.send_message(chat_id=update.effective_chat.id, text='Введите год рождения',
+                                     reply_markup=reply_markup)
 
+    # условие обработки ввода года рождения
     elif context.user_data['stage'] == 'awaiting_birth_age':
         if text == 'Пропустить':
-            context.user_data['birth_age'] = None
-            context.user_data['stage'] = 'awaiting_birth_month'
-
-            keyboard = [
-                [InlineKeyboardButton(m, callback_data=m) for m in ["Январь", "Февраль", "Март"]],
-                [InlineKeyboardButton(m, callback_data=m) for m in ["Апрель", "Май", "Июнь"]],
-                [InlineKeyboardButton(m, callback_data=m) for m in ["Июль", "Август", "Сентябрь"]],
-                [InlineKeyboardButton(m, callback_data=m) for m in ["Октябрь", "Ноябрь", "Декабрь"]],
-            ]
-
-            reply_markup = InlineKeyboardMarkup(keyboard)
-            context.bot.send_message(chat_id=update.effective_chat.id, text='Выберите месяц рождения',
-                                     reply_markup=reply_markup)
-
-        elif not text.isdigit() or not 1901 <= int(text) <= 2024:
-            context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text='Неверный формат данных. Пожалуйста введите год от 1901 до 2024. Пример: 1991')
-
-        else:
+            context.user_data['birth_age'] = 1900
+        elif text.isdigit() and 1901 <= int(text) <= datetime.now().year:
             context.user_data['birth_age'] = int(text)
-            context.user_data['stage'] = 'awaiting_birth_month'
-            keyboard = [
-                [InlineKeyboardButton(m, callback_data=m) for m in ["Январь", "Февраль", "Март"]],
-                [InlineKeyboardButton(m, callback_data=m) for m in ["Апрель", "Май", "Июнь"]],
-                [InlineKeyboardButton(m, callback_data=m) for m in ["Июль", "Август", "Сентябрь"]],
-                [InlineKeyboardButton(m, callback_data=m) for m in ["Октябрь", "Ноябрь", "Декабрь"]],
-            ]
+        else:
+            context.bot.send_message(chat_id=update.effective_chat.id,
+                                     text='Введите год вашего рождения в формате ГГГГ.')
+            return
 
-            reply_markup = InlineKeyboardMarkup(keyboard)
-            context.bot.send_message(chat_id=update.effective_chat.id, text='Выберите месяц рождения',
-                                     reply_markup=reply_markup)
+        context.user_data['stage'] = 'awaiting_birth_month'
+        context.user_data['is_leap'] = is_leap(context.user_data['birth_age'])
 
-    elif context.user_data['stage'] == 'awaiting_birth_month':
-        context.user_data['birth_month'] = text
-        context.user_data['stage'] = 'awaiting_birth_date'
-        context.bot.send_message(chat_id=update.effective_chat.id, text='Введите день рождения')
+        keyboard = [[InlineKeyboardButton(m, callback_data=m)] for m in
+                    ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь",
+                     "Октябрь", "Ноябрь", "Декабрь"]]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Выберите месяц рождения',
+                                 reply_markup=reply_markup)
 
+    # условие обработки ввода даты рождения
     elif context.user_data['stage'] == 'awaiting_birth_date':
         month_days = {
             'Апрель': 30, 'Июнь': 30, 'Сентябрь': 30, 'Ноябрь': 30,
             'Январь': 31, 'Март': 31, 'Май': 31, 'Июль': 31, 'Август': 31, 'Октябрь': 31, 'Декабрь': 31
         }
-        month_days['Февраль'] = 29 if context.user_data['birth_age'] % 4 == 0 and (
-            context.user_data['birth_age'] % 100 != 0 or context.user_data['birth_age'] % 400 == 0) else 28
+        month_days['Февраль'] = 29 if context.user_data['is_leap'] else 28
 
         if not text.isdigit() or not 1 <= int(text) <= month_days[context.user_data['birth_month']]:
             context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text=f'Неверный формат данных. В выбранном месяце {month_days[context.user_data["birth_month"]]} дней. Пример: {min(7, month_days[context.user_data["birth_month"]])}')
-
+                                     text=f'Неверный формат данных. В выбранном месяце {month_days[context.user_data["birth_month"]]} дней.')
         else:
-            context.user_data['birth_date'] = int(text)
+            month_list = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+                          "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
+            # формирование даты рождения - получаем год, месяц и день, затем создаем объект date
+            birth_year = context.user_data.get('birth_age')
+            birth_month = month_list.index(
+                context.user_data.get('birth_month')) + 1  # учет того, что в Python отсчет начинается с 0
+            birth_day = int(text)
+            context.user_data['birth_date'] = date(birth_year, birth_month, birth_day)
             context.user_data['stage'] = 'awaiting_sex'
+
             keyboard = [
                 [InlineKeyboardButton(option, callback_data=option) for option in ['М', 'Ж']],
                 [InlineKeyboardButton("Пропустить", callback_data='Пропустить')]
             ]
-
             reply_markup = InlineKeyboardMarkup(keyboard)
             context.bot.send_message(chat_id=update.effective_chat.id, text='Выберите пол', reply_markup=reply_markup)
