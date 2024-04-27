a79d6895ba7d452db906b4d3b6073deb1b5a4c20
Vladimir Moiseev <memmfizwork@gmail.com>
2024-04-27 09:04:52 +0300

    Fixed bug with missing last_modified updating while send a notification

--- main/notifications/notify.py
+++ main/notifications/notify.py
@@ -11,6 +11,7 @@ Session = sessionmaker(bind=engine)
 def send_notification(bot: Bot, notification: Notification, session):
     bot.send_message(chat_id=notification.user_telegram_id, text=notification.notification_text)
     notification.notification_status = 'SENT'
+    notification.lastmodified = datetime.datetime.now()
     session.commit()
 
 def scheduler_for_notifications(bot_token: str):
