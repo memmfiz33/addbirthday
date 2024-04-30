b7f4124cab373092889c4448335efb08f6a3614f
Vladimir Moiseev <memmfizwork@gmail.com>
2024-04-30 16:10:24 +0300

    Add notification for old records

--- main/notifications/notify.py
+++ main/notifications/notify.py
@@ -3,12 +3,20 @@ import datetime
 from telegram import Bot
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from databaseOperations.models import Notification
+from databaseOperations.models import Notification, Birthdays
 
 engine = create_engine('postgresql://postgres:postgres@localhost:5432/addbirthday')
 Session = sessionmaker(bind=engine)
 
 def send_notification(bot: Bot, notification: Notification, session):
+    now = datetime.datetime.now()
+    time_difference = now - notification.scheduled_time
+    if time_difference.total_seconds() > 15 * 60 * 60:  # 15 hours in seconds
+        month_names = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"]
+        birthday = session.query(Birthdays).filter(Birthdays.id == notification.birthdays_id).first()
+        if birthday:
+            birth_person = birthday.birth_person
+            notification.notification_text = f"{notification.birth_date.day} {month_names[notification.birth_date.month - 1]} был день рождения у контакта {birth_person}. Можете поздравить, если это еще актуально"
     bot.send_message(chat_id=notification.user_telegram_id, text=notification.notification_text)
     notification.notification_status = 'SENT'
     notification.lastmodified = datetime.datetime.now()
