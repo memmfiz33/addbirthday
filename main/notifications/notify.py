3cfd9f0552c932eab69c47dc9a9478705cabf8fc
Vladimir Moiseev <memmfizwork@gmail.com>
2024-05-06 21:39:32 +0300

    Added logger for notify.py. Added ERROR status if message did not send

--- main/notifications/notify.py
+++ main/notifications/notify.py
@@ -1,23 +1,28 @@
 import time
 import datetime
 from telegram import Bot
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from databaseOperations.models import Notification, Birthdays, get_session
+from logger.logger import logger  # Импортируйте настроенный логгер
 
 def send_notification(bot: Bot, notification: Notification, session):
-    now = datetime.datetime.now()
-    time_difference = now - notification.scheduled_time
-    if time_difference.total_seconds() > 15 * 60 * 60:  # 15 hours in seconds
-        month_names = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"]
-        birthday = session.query(Birthdays).filter(Birthdays.id == notification.birthdays_id).first()
-        if birthday:
-            birth_person = birthday.birth_person
-            notification.notification_text = f"{notification.birth_date.day} {month_names[notification.birth_date.month - 1]} был день рождения у контакта {birth_person}. Можете поздравить, если это еще актуально"
-    bot.send_message(chat_id=notification.user_telegram_id, text=notification.notification_text)
-    notification.notification_status = 'SENT'
-    notification.lastmodified = datetime.datetime.now()
-    session.commit()
+    try:
+        now = datetime.datetime.now()
+        time_difference = now - notification.scheduled_time
+        if time_difference.total_seconds() > 15 * 60 * 60:  # 15 hours in seconds
+            month_names = ["Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"]
+            birthday = session.query(Birthdays).filter(Birthdays.id == notification.birthdays_id).first()
+            if birthday:
+                birth_person = birthday.birth_person
+                notification.notification_text = f"{notification.birth_date.day} {month_names[notification.birth_date.month - 1]} был день рождения у контакта {birth_person}. Можете поздравить, если это еще актуально"
+        bot.send_message(chat_id=notification.user_telegram_id, text=notification.notification_text)
+        notification.notification_status = 'SENT'
+        notification.lastmodified = datetime.datetime.now()
+        session.commit()
+        logger.info(f'Successfully sent notification to {notification.user_telegram_id}')
+    except Exception as e:
+        notification.notification_status = 'ERROR'
+        session.commit()
+        logger.error(f'Failed to send notification to {notification.user_telegram_id} due to {e}')
 
 def scheduler_for_notifications(bot_token: str):
     bot = Bot(bot_token)
