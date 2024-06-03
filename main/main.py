528ac5798eaf70467346d86355c66a44e5e00240
Vladimir Moiseev <memmfizwork@gmail.com>
2024-06-03 21:56:39 +0300

    Refactored buttons for AI generation

--- main/main.py
+++ main/main.py
@@ -1,5 +1,4 @@
 import sys
-
 sys.path.append('/home/memmfiz_admin/addbirthday/main')
 from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler, Filters
 from handlers import start_command, addbirthday_command, info_command, handle_message, handle_button, delete_command
@@ -13,18 +12,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 from notifications.support_notifications import create_support_notifications, start_support_notifications_scheduler
-# from AI.gpt_request import generate_birthday_message  # Уберите этот импорт, если он больше не нужен
+from AI.ai_buttons import generate_message, handle_generate_callback
 
 import threading
 import os
 
 TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 
-
 def main() -> None:
     logger.info('Starting bot....')
-    # Уберите или закомментируйте этот вызов
-    # generate_birthday_message()
 
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
@@ -34,10 +30,13 @@ def main() -> None:
     dp.add_handler(CommandHandler('showall', showall_command))
     dp.add_handler(CommandHandler('delete', delete_command))
     dp.add_handler(CommandHandler('support', support_command))  # добавьте обработчик команды /support
-    dp.add_handler(
-        MessageHandler(Filters.text & Filters.reply, handle_support))  # добавьте обработчик ответа на команду /support
+    dp.add_handler(MessageHandler(Filters.text & Filters.reply, handle_support))  # добавьте обработчик ответа на команду /support
     dp.add_handler(CallbackQueryHandler(handle_button))
     dp.add_handler(MessageHandler(Filters.text & (~Filters.command), handle_message))
+
+    # Обработчики для генерации сообщений AI
+    dp.add_handler(CallbackQueryHandler(generate_message, pattern='^generate_message$'))
+    dp.add_handler(CallbackQueryHandler(handle_generate_callback, pattern='^generate:'))
 
     # нотификации
     notification_thread = threading.Thread(target=scheduler_for_notifications, args=(TOKEN,), daemon=True)
@@ -56,7 +55,6 @@ def main() -> None:
 
     updater.start_polling()
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
