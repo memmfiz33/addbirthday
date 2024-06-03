319ec42ed6c7e3f44bb1a9a2edf7f4ddf722fbda
Vladimir Moiseev <memmfizwork@gmail.com>
2024-06-03 23:59:13 +0300

    Fixed issue with record creation

--- main/handlers/start.py
+++ main/handlers/start.py
@@ -1,20 +1,17 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CallbackContext
 
-
 def start_command(update: Update, context: CallbackContext) -> None:
     keyboard = [
         [InlineKeyboardButton('🎂 Добавить ДР!', callback_data='addbirthday')],
         [InlineKeyboardButton('📖 Показать все записи', callback_data='showall')],
         [InlineKeyboardButton('🗑️ Удалить запись', callback_data='delete')],
-        [InlineKeyboardButton('🎉 Создать поздравление', callback_data='generate_message')],  # Добавляем эту строку
+        [InlineKeyboardButton('🎉 Создать поздравление', callback_data='generate_message')],
         [InlineKeyboardButton('💡 Инфо и помощь', callback_data='info')]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    # Если это обновление сообщения, используем update.message
     if update.message is not None:
         update.message.reply_text('Выберите действие:', reply_markup=reply_markup)
-    # Если это обновление callback-запроса, используем update.callback_query.message
     elif update.callback_query is not None:
         update.callback_query.message.reply_text('Выберите действие:', reply_markup=reply_markup)
